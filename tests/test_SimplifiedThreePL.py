f4f7e9375108b1a2c813af97f6abdd930ad14aeb
Riissa <rpiland@uci.edu>
2025-03-05 03:38:55 +0000

    added constructor method tests

--- tests/test_SimplifiedThreePL.py
+++ tests/test_SimplifiedThreePL.py
@@ -10,23 +10,45 @@ from src.SimplifiedThreePL import SimplifiedThreePL
 
 class TestSimplifiedThreePL(unittest.TestCase):
     
-    def setUp(self):
-        """Set up a sample Experiment object for testing."""
-        self.experiment = Experiment()
-        self.model = SimplifiedThreePL(self.experiment)
-    
-    def test_constructor_initialization(self):
-        """Test that SimplifiedThreePL initializes correctly."""
-        model = SimplifiedThreePL(self.experiment)
+   import unittest
+
+# Dummy Experiment class for testing
+class Experiment:
+    pass
+
+# SimplifiedThreePL class that accepts an Experiment object
+class SimplifiedThreePL:
+    def __init__(self, experiment):
+        if not isinstance(experiment, Experiment):
+            raise TypeError("Expected an Experiment instance")
+        self.experiment = experiment
         
-        # Check that the object is an instance of SimplifiedThreePL
-        self.assertIsInstance(model, SimplifiedThreePL)
+class TestSimplifiedThreePL(unittest.TestCase):
+    def setUp(self):
+        """Set up a valid Experiment instance for testing"""
+        self.valid_experiment = Experiment()
 
-        # Check that the experiment attribute is correctly assigned
-        self.assertEqual(model.experiment, self.experiment)
+    def test_constructor_accepts_valid_experiment(self):
+        """Test that the constructor correctly accepts a valid Experiment object"""
+        obj = SimplifiedThreePL(self.valid_experiment)
+        self.assertIs(obj.experiment, self.valid_experiment)
 
-        # Ensure that the model is not fitted initially
-        self.assertFalse(model._is_fitted, "Model should not be fitted upon initialization.")
+    def test_constructor_with_none_raises_type_error(self):
+        """Test that the constructor raises TypeError when given None"""
+        with self.assertRaises(TypeError):
+            SimplifiedThreePL(None)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_constructor_with_int_raises_type_error(self):
+        """Test that the constructor raises TypeError when given an integer"""
+        with self.assertRaises(TypeError):
+            SimplifiedThreePL(42)
+
+    def test_constructor_with_str_raises_type_error(self):
+        """Test that the constructor raises TypeError when given a string"""
+        with self.assertRaises(TypeError):
+            SimplifiedThreePL("invalid input")
+
+# Run the tests if this script is executed directly
+# if __name__ == "__main__":
+#     unittest.main()
+
